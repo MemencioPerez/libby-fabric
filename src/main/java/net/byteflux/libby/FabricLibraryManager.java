3cb8c65941fb2010a49805675db5a806f982e61c
MemencioPerez <memencio.perez.2004@gmail.com>
2025-07-16 00:25:21 -0400

    misc: Remove unused code

--- src/main/java/net/byteflux/libby/FabricLibraryManager.java
+++ src/main/java/net/byteflux/libby/FabricLibraryManager.java
@@ -5,24 +5,21 @@ import net.fabricmc.loader.impl.launch.FabricLauncherBase;
 import org.slf4j.Logger;
 
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 
 /**
  * A runtime dependency manager for Fabric mods.
  */
-public class FabricLibraryManager<T> extends LibraryManager {
+public class FabricLibraryManager extends LibraryManager {
     /**
      * Creates a new Fabric library manager.
      *
      * @param logger        the mod logger
      * @param dataDirectory mod's data directory
-     * @param mod        the mod to manage
      * @param directoryName download directory name
      */
     public FabricLibraryManager(Logger logger,
                                 Path dataDirectory,
-                                T mod,
                                 String directoryName) {
 
         super(new FabricLogAdapter(logger), dataDirectory, directoryName);
@@ -33,12 +30,10 @@ public class FabricLibraryManager<T> extends LibraryManager {
      *
      * @param logger        the mod logger
      * @param dataDirectory mod's data directory
-     * @param mod        the mod to manage
      */
     public FabricLibraryManager(Logger logger,
-                                  Path dataDirectory,
-                                  T mod) {
-        this(logger, dataDirectory, mod, "lib");
+                                  Path dataDirectory) {
+        this(logger, dataDirectory, "lib");
     }
 
     /**
@@ -54,15 +49,5 @@ public class FabricLibraryManager<T> extends LibraryManager {
     @Override
     protected InputStream getPluginResourceAsInputStream(String path) {
         return getClass().getClassLoader().getResourceAsStream(path);
-    }
-
-    private ClassLoader getDynamicURLClassLoader(ClassLoader classLoader) {
-        try {
-            Field urlLoaderField = classLoader.getClass().getDeclaredField("urlLoader");
-            urlLoaderField.setAccessible(true);
-            return (ClassLoader) urlLoaderField.get(classLoader);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
