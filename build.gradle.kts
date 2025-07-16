d44fa3958410f4e1b7510e1804801e3a48d3424a
MemencioPerez <memencio.perez.2004@gmail.com>
2025-07-15 22:11:18 -0400

    fix: set target Java version to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,25 @@ dependencies {
     implementation("xyz.kyngs.libby:libby-core:1.7.0")
 }
 
+val targetJavaVersion = 11
+tasks.compileJava {
+    // ensure that the encoding is set to UTF-8, no matter what the system default is
+    // this fixes some edge cases with special characters not displaying correctly
+    // see http://yodaconditions.net/blog/fix-for-java-file-encoding-problems-with-gradle.html
+    // If Javadoc is generated, this must be specified in that task too.
+    options.encoding = "UTF-8"
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release = targetJavaVersion
+    }
+}
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
+}
+
 tasks.assemble {
     dependsOn(tasks.shadowJar)
 }
